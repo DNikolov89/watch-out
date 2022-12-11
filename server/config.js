cd03a488d28624dd5af933ac8aac80c58460da4b
Dimitar Nikolov <d.n.nikolov@protonmail.com>
2022-12-11 20:22:51 +0200

    [server] add index and config files

---
+++ server/config.js
@@ -0,0 +1,10 @@
+const config = {
+
+    port: 5000,
+    DB_CONNECTION: 'mongodb+srv://Dimitar:angular@watch-out.kyrur.mongodb.net/test?authSource=admin&replicaSet=atlas-r2gn9x-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    COOKIE_NAME: 'user_cookie',
+    SALT_ROUNDS: 10,
+    JWT_SECRET: 'angulartop',
+};
+
+export default config;
