ff3f3f75fd879745202b1285ec91cfe6897c2116
Dimitar Nikolov <d.n.nikolov@protonmail.com>
2022-12-12 07:34:13 +0200

    Add footer component

--- angular-watch-out/src/app/app.module.ts
+++ angular-watch-out/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
