d5cddbb6e40824f3d2da4ef69e2cf86f2dcba598
Dimitar Nikolov <d.n.nikolov@protonmail.com>
2022-12-12 08:28:22 +0200

    Add routing and routes

--- angular-watch-out/src/app/app-routing.module.ts
+++ angular-watch-out/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SellWatchComponent } from './components/sell-watch/sell-watch.component';
+import { WatchesComponent } from './components/watches/watches.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent
+  },
+  {
+    path: 'watches',
+    component: WatchesComponent
+  },
+  {
+    path: 'sell-watch',
+    component: SellWatchComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
