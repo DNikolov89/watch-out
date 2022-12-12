7b44be029d3dee995b36da4adeeb23db2b462990
Dimitar Nikolov <d.n.nikolov@protonmail.com>
2022-12-12 07:36:37 +0200

    Add error page component

--- angular-watch-out/src/app/app.module.ts
+++ angular-watch-out/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
